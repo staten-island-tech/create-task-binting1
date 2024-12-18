6e7eb5e8c9d7cfb9b4470ed91f5c1789119eb871
binting1 <bintingchenn123@gmail.com>
2024-12-18 14:00:52 -0500

    123

--- app/js/desserts.js
+++ app/js/desserts.js
@@ -1,11 +1,24 @@
-
 const desserts = [
     {
-        element: "Blueberry Cake",
+        name: "Blueberry Cake",
+        answer: ["calm", "quiet", "resilent", "sensitive"]
     },
     {
-        element: "Strawberry Cake",
+        name: "Strawberry Cake",
+        answer: ["sweet", "outgoing", "energetic", "stubborn"]
     },
+    {
+        name: "Tiramisu", 
+        answer: ["serious", "bittersweet", "on-time", "empathetic"]
+    }, 
+    {
+        name: "Cherry Pie",
+        answer: ["warm", "caring", "detailed", "mature"] 
+    },
+    {
+        name: "Creme Brulee", 
+        answer: ["bubbly", "ambivert", "chaotic", "good vibes"]
+    }
 ];
 
 export {desserts};
--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,28 @@
-import './style.css'
-import {desserts} from '/js/desserts.js'
+import '/src/style.css'
+import { desserts } from '/js/desserts.js'
+
+const DOMSelectors = {
+    start:document.querySelector(".start"),
+    container: document.querySelector(".container"),
+
+};
 
 function createCard (desserts) {
-    
-}
+    desserts.forEach((desserts) => DOMSelectors.container.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+            <h2 class="title">${desserts.name}</h2>
+
+        </div>`
+    ));
+};
+
+createCard (desserts); 
+
+// function beginningPage () {
+//     DOMSelectors.start.insertAdjacentHTML("beforeend", 
+//         `<div class="beginning">
+//             <h2></h2>
+//         </div>`
+//     )
+// }
+
