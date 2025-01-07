f8d156edf9c9476af82e4b3e96fce4c22e9eb041
binting1 <bintingchenn123@gmail.com>
2025-01-07 13:59:47 -0500

    123

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,12 @@
 import '/src/style.css'
-import { desserts } from '/js/desserts.js'
 import {questions} from '/js/questions.js'
 
 const DOMSelectors = {
-    start:document.querySelector(".start"),
     container: document.querySelector(".container"),
-
 };
 
 function startingPage () {
-    DOMSelectors.start.insertAdjacentHTML("beforeend", 
+        DOMSelectors.container.insertAdjacentHTML("beforeend", 
         `<div class="play"> 
             <h2 class="title">Which Dessert Are You?</h2>
             <button class="btn">start</button>
@@ -20,16 +17,16 @@ function startingPage () {
 startingPage (); 
 
 document.querySelector(".btn").addEventListener("click", function (){
-    DOMSelectors.start.innerHTML = "";
+    DOMSelectors.container.innerHTML = "";
   
     questions.forEach((question) => DOMSelectors.container.insertAdjacentHTML("beforeend", 
         `<div class="play"> 
             <h2 class="questions">${question.prompt}</h2>
-            <button class="blueberry">${questions.answer}</button>
-            <button class="strawberry">${questions.answer}</button>
-            <button class="tiramisu">${questions.answer}</button>
-            <button class="cherry">${questions.answer}</button>
-            <button class="creme brulee">${questions.answer}</button>
+            <button class="blueberry">${answer.text}</button>
+            <button class="strawberry">${questions.answer.text}</button>
+            <button class="tiramisu">${questions.answer.text}</button>
+            <button class="cherry">${questions.answer.text}</button>
+            <button class="creme brulee">${questions.answer.text}</button>
         </div>`
     ));
     DOMSelectors.container.insertAdjacentHTML("beforeend", 
@@ -38,12 +35,15 @@ document.querySelector(".btn").addEventListener("click", function (){
         <button class="submit">Submit</button>
         `
     );
+
+    document.querySelector(".restart").addEventListener("click", function (){
+        DOMSelectors.container.innerHTML='';
+        //create a function to clear fields
+    });
+
 });
 
-document.querySelector(".restart").addEventListener("click", function () {
-    DOMSelectors.container.innerHTML = "";
-    startingPage ();
-});
+
 
 //get rid of start div to see if you can use container div only instead to return to starting page
 //come up with one more question 
@@ -51,33 +51,6 @@ document.querySelector(".restart").addEventListener("click", function () {
 //use algorithm to calculate the final result 
 //finish writing descriptions for each dessert 
 
-//question 1: where would you spend your weekend if you had no work or projects to worry about 
-//blueberry: the beach 
-//strawberry: spend time with friends 
-//tiramisu: movie theater 
-//cherry pie: at home 
-//creme brulee: spa center 
-
-//question 2: what would you do if your friend had an argument with you?
-//blueberry: confront them and get it over with 
-//strawberry: offer altneratives to the problem 
-//tiramisu: wait for them to reach out 
-//cherry pie: try to understand them and soothe out their emotions 
-//creme brulee: ask someone else to resolve the problem 
-
-//question 3: what would you do if you forgot to study for an upcoming test the night before?
-//blueberry: study to your max capibility and stop when you start to feel overwhelmed because of the workload 
-//strawberry: reach out to friends to study together 
-//tiramisu: worry more about the time than actually doing the studying 
-//cherry pie: ends up staying all night studying for the test 
-//creme brulee: gives up on it because it's already too late in your opinion 
-
-//question 4: Which format do you prefer the most when working on a persuasive argument?
-//blueberry: providing various stats and numbers to prove your point 
-//strawberry: practice speech to sound persuasive 
-//tiramisu: in the most simplistic and neat way , as long as it gets the job done 
-//cherry pie: carefuly checks all the rubric and work based off of that 
-//creme brulee: providing creative artworks to get your inner voice across 
 
 //blue: you are very calm yet earnest.You have a fascinating inner world and you know  
 //what you want and is eager to get your points across. So you refuse any interactions 
--- app/js/questions.js
+++ app/js/questions.js
@@ -2,16 +2,53 @@
 const questions = [
     {
         prompt:"where would you spend your weekend if you had no work or projects to worry about?",
-            answer: ["calm", "quiet", "resilent", "sensitive"],
+        answer: [
+            { dessert: "blueberry", text:"the beach"},
+            { dessert: "strawberry", text:"spend time with friends"},
+            { dessert: "tiramisu", text:"movie theater"},
+            { dessert: "cherry pie", text:"at home"},
+            { dessert: "creme brulee", text:"spa center"},
+            ],
     },
     {
         prompt: "what would you do if your friend had an argument with you?",
+        answer: [
+            { dessert: "blueberry", text:"confront them and get it over with "},
+            { dessert: "strawberry", text:"offer altneratives to the problem "},
+            { dessert: "tiramisu", text:" wait for them to reach out "},
+            { dessert: "cherry pie", text:"try to understand them and soothe out their emotions "},
+            { dessert: "creme brulee", text:"ask someone else to resolve the problem "},
+            ],
     },
     {
         prompt: "what would you do if you forgot to study for an upcoming test the night before?",
+        answer: [
+            { dessert: "blueberry", text:"study to your max capibility and stop when you start to feel overwhelmed because of the workload "},
+            { dessert: "strawberry", text:"reach out to friends to study together "},
+            { dessert: "tiramisu", text:"worry more about the time than actually doing the studying "},
+            { dessert: "cherry pie", text:"ends up staying all night studying for the test "},
+            { dessert: "creme brulee", text:"the beach"},
+        ],
     },
     {
         prompt: "Which format do you prefer the most when working on a persuasive argument?",
+        answer: [
+            { dessert: "blueberry", text:"providing various stats and numbers to prove your point "},
+            { dessert: "strawberry", text:"practice speech to sound persuasive "},
+            { dessert: "tiramisu", text:"in the most simplistic and neat way , as long as it gets the job done "},
+            { dessert: "cherry pie", text:"carefuly checks all the rubric and work based off of that "},
+            { dessert: "creme brulee", text:"providing creative artworks to get your inner voice across "},
+            ],
+    },
+    {
+        prompt: "What time of the day are you the most energetic?",
+        answer: [
+            { dessert: "blueberry", text:"evening"},
+            { dessert: "strawberry", text:"all day"},
+            { dessert: "tiramisu", text:"never energetic"},
+            { dessert: "cherry pie", text:"in the morning"},
+            { dessert: "creme brulee", text:"at night"},
+            ],
     },
    
 ]
